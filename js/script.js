3217a5629c26f243437fb6d7333db385471aca2d
Lana <slomteva@outlook.com>
2021-01-28 17:33:54 +0300

    contact form

--- js/script.js
+++ js/script.js
@@ -9,3 +9,8 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 })
+
+const counters = document.querySelectorAll('.skills__progress-counter'),
+    lines = document.querySelectorAll('.skills__progress-line span');
+
+counters.forEach((item, i) => {lines[i].style.width = item.innerHTML})
